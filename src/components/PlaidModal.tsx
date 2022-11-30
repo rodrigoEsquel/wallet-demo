c4f61dc3d7631bd3810b1c222642672b86d9fbfb
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-11-30 14:04:49 -0300

    Updates PlaidModal

--- src/components/PlaidModal.tsx
+++ src/components/PlaidModal.tsx
@@ -1,33 +1,44 @@
-import { useState, useEffect, useCallback } from 'react';
-import { View, Button, Text } from 'react-native';
-import { PlaidLink, LinkExit, LinkSuccess } from 'react-native-plaid-link-sdk';
+import { useState } from 'react';
+import { View, Button, Text, FlatList } from 'react-native';
+import { PlaidLink, LinkSuccess } from 'react-native-plaid-link-sdk';
 
-const IP = process.env.IP || 'localhost';
+const IP = process.env.IP || '192.168.0.25';
 const PORT = process.env.PORT || '5000';
 
 export default function PlaidModal() {
-  const [linkToken, setLinkToken] = useState('');
+  const [linkToken, setLinkToken] = useState('empty token');
+  const [steps, setSteps] = useState<{ message: string }[]>([]);
+
+  const addStep = (newStep: string) => {
+    setSteps((prev) => [...prev, { message: newStep }]);
+  };
 
   const createLinkToken = async (userDetails: { userId: string }) => {
-    const response = await fetch(
-      `http://${IP}:${PORT}/api/token/create-link-token`,
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+    addStep('Fetch Link Token to ' + IP + ':' + PORT);
+    try {
+      const response = await fetch(
+        `http://${IP}:${PORT}/api/token/create-link-token`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userDetails),
         },
-        body: JSON.stringify(userDetails),
-      },
-    );
-    const responseJSON = await response.json();
-    console.log('response', responseJSON.link_token);
-    setLinkToken(responseJSON.link_token);
+      );
+      const responseJSON = await response.json();
+      addStep('token recieved: ' + responseJSON.link_token);
+      setLinkToken(responseJSON.link_token);
+    } catch (error) {
+      addStep('error fetchnig the backend');
+    }
     //return responseJSON.link_token;
   };
 
-  const createProcesorToken = useCallback(
-    async (successResponse: LinkSuccess) => {
+  const createProcesorToken = async (successResponse: LinkSuccess) => {
+    addStep('Fetch Processor Token');
+    try {
       fetch(`http://${IP}:${PORT}/api/token/create-processor-token`, {
         method: 'POST',
         body: JSON.stringify({
@@ -35,9 +46,11 @@ export default function PlaidModal() {
           institution: successResponse.metadata.institution,
         }),
       });
-    },
-    [linkToken],
-  );
+      addStep('Processor Token sended');
+    } catch (error) {
+      addStep('error fetchnig the backend');
+    }
+  };
   /*
   useEffect(() => {
     if (linkToken == null) {
@@ -52,24 +65,33 @@ export default function PlaidModal() {
         title="Get Token"
         onPress={() => createLinkToken({ userId: '1' })}
       />
+      <Button title="Clear Log" onPress={() => setSteps([])} />
       <Text>{linkToken}</Text>
       <PlaidLink
         tokenConfig={{
           token: linkToken,
           noLoadingState: false,
         }}
-        onPress={() => console.log(linkToken)}
+        onPress={() => addStep('Link Pressed')}
         onSuccess={(success: LinkSuccess) => {
-          console.log(success);
           createProcesorToken(success);
         }}
         onExit={(exit) => {
-          setLinkToken('');
-          console.log(exit);
+          setLinkToken('empty token');
+          addStep('Link Exited');
         }}>
-        <Text>Select Account</Text>
+        <Text
+          style={{
+            backgroundColor: 'red',
+            textAlign: 'center',
+          }}>
+          Select Account
+        </Text>
       </PlaidLink>
+      <FlatList
+        data={steps}
+        renderItem={({ item }) => <Text>{item.message} </Text>}
+      />
     </View>
   );
 }
-
